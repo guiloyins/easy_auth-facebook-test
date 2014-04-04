09193910cdb5dff211e7b5db116f19f10909d768
Rafael Fiuza <rafael.fiuza@helabs.com.br>
2014-04-04 10:52:39 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+POmniauthFacebookRails::Application.config.secret_key_base = 'd156c8ba7adea43b2904884f6e9dc47981a91bb062198f4b4d64689d61053d965485b9a198f233d2e03ac549a392e7d3e17b959f877e52dcd752d47ea05868fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+POmniauthFacebookRails::Application.config.session_store :cookie_store, key: '_p_omniauth-facebook-rails_session'
