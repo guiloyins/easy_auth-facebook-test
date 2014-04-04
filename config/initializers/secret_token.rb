90f1ec2afd46171020bc522e5bbf05eb1a67c772
Rafael Fiuza <rafael.fiuza@helabs.com.br>
2014-04-04 10:53:47 -0300

    Replace secret token with environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-POmniauthFacebookRails::Application.config.secret_key_base = 'd156c8ba7adea43b2904884f6e9dc47981a91bb062198f4b4d64689d61053d965485b9a198f233d2e03ac549a392e7d3e17b959f877e52dcd752d47ea05868fc'
+POmniauthFacebookRails::Application.config.secret_key_base = ENV['SECRET_TOKEN']
